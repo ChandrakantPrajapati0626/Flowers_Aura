018e19569fed8ad05986d61eaf13fdf5003248eb
Mahesh Bhongal <mbhongal17@gmail.com>
2023-03-26 00:10:13 -0700

    ReDesign code of Pizza

---
+++ Css/pizza.css
@@ -0,0 +1,87 @@
+ body{
+            background-color: aquamarine;
+        }
+    form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-top: 50px;
+    }
+    label {
+      font-size: 40px;
+      margin-bottom: 10px;
+    }
+    select {
+      font-size: 25px;
+      padding: 5px;
+      /* margin-left: 40px; */
+      margin-bottom: 20px;
+    }
+    button {
+      font-size: 30px;
+      padding: 10px 20px;margin-left: 800px;
+      background-color: #4CAF50;
+      color: white;
+      margin-left: 0px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+    button:hover {
+      background-color: #3e8e41;
+    }
+    /* CSS styling for the image display */
+    .image-container {
+      display: flex;
+      justify-content: center;
+      margin-top: 10px;
+     margin-left: 650px;
+      height: 600px;
+      width: 600px;
+      background-color: rgb(198, 249, 255);
+      border-radius: 50px;
+      box-shadow: 1px 1px 2px 2px black;
+    }
+    img {
+      max-width: 560px;
+      max-height: 560px;
+      margin-top: 18px;
+      border-radius: 20px;
+      cursor: pointer;
+    }
+    img:hover{
+        box-shadow: 2px 2px 3px 3px black;
+    }
+
+    /* CSS styling for the input field */
+    .input-field {
+      display: flex;
+      flex-direction: column;
+      
+      align-items: center;
+      margin-top: 20px;
+    }
+    input[type="text"] {
+      font-size: 16px;
+      padding: 5px;
+      margin-bottom: 10px;
+      width: 26%;
+    }
+    /* .category-name{
+        display: inline-block;
+        margin-left: 700px;
+    } */
+
+    #button {
+      font-size: 30px;
+      padding: 10px 20px;margin-left: 800px;
+      background-color: #4CAF50;
+      color: white;
+      margin-left: 790px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+    #button:hover {
+      background-color: #3e8e41;
+    }
