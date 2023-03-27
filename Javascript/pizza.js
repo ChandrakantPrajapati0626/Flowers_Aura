b6418f9520696cdc67b752730e8ff93b55f79e66
Mahesh Bhongal <mbhongal17@gmail.com>
2023-03-27 07:24:15 -0700

    Javascript page added

--- Javascript/pizza.js
+++ Javascript/pizza.js
@@ -1,16 +1,16 @@
 
 // JavaScript code to generate random images
 function generateImage() {
-   const category = document.getElementById('category').value;
-   const num = Math.floor(Math.random() * 10) + 1; // Generate a random number from 1 to 10
-   const imageUrl = `https://source.unsplash.com/500x500/?${category}&sig=${num}`;
-   document.getElementById('image').src = imageUrl;
-   document.getElementById('image-url').value = imageUrl;
- }
- // JavaScript code to copy image URL
- function copyImageUrl() {
-   const imageUrl = document.getElementById('image-url').value;
-   navigator.clipboard.writeText(imageUrl);
-   alert('Image URL copied to clipboard!');
- }
- 
+    const category = document.getElementById('category').value;
+    const num = Math.floor(Math.random() * 10) + 1; // Generate a random number from 1 to 10
+    const imageUrl = `https://source.unsplash.com/500x500/?${category}&sig=${num}`;
+    document.getElementById('image').src = imageUrl;
+    document.getElementById('image-url').value = imageUrl;
+  }
+  // JavaScript code to copy image URL
+  function copyImageUrl() {
+    const imageUrl = document.getElementById('image-url').value;
+    navigator.clipboard.writeText(imageUrl);
+    alert('Image URL copied to clipboard!');
+  }
+  
